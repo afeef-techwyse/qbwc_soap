492b560a3f059152cf94b641e75397ecdd814ffe
afeef-techwyse <afeef.techintl@gmail.com>
2025-07-27 23:53:26 +0530

    add

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, Response
+from spyne import Application, rpc, ServiceBase, Unicode, Integer
+from spyne.protocol.soap import Soap11
+from spyne.server.wsgi import WsgiApplication
+
+class QBWebConnectorService(ServiceBase):
+    @rpc(Unicode, Unicode, _returns=(Unicode, Integer))
+    def authenticate(ctx, username, password):
+        if username == "myusername" and password == "mypassword":
+            return ["12345", 1]
+        else:
+            return ["", 0]
+
+    @rpc(Unicode, _returns=Unicode)
+    def sendRequestXML(ctx, ticket):
+        return '''<?xml version="1.0"?>
+        <QBXML>
+          <QBXMLMsgsRq onError="stopOnError">
+            <CustomerQueryRq requestID="1">
+              <MaxReturned>10</MaxReturned>
+            </CustomerQueryRq>
+          </QBXMLMsgsRq>
+        </QBXML>'''
+
+    @rpc(Unicode, Unicode, _returns=Integer)
+    def receiveResponseXML(ctx, ticket, response):
+        print("QB Response:", response)
+        return 100
+
+    @rpc(Unicode, _returns=Unicode)
+    def getLastError(ctx, ticket):
+        return "No error"
+
+    @rpc(Unicode, _returns=Unicode)
+    def closeConnection(ctx, ticket):
+        return "Goodbye!"
+
+soap_app = Application([QBWebConnectorService],
+                       tns='qbwc.soap',
+                       in_protocol=Soap11(validator='lxml'),
+                       out_protocol=Soap11())
+
+wsgi_app = WsgiApplication(soap_app)
+
+app = Flask(__name__)
+@app.route("/qbwc", methods=["POST"])
+def soap_interface():
+    return Response(wsgi_app(request.environ, start_response=lambda status, headers: None),
+                    mimetype="text/xml")
+
+@app.route("/")
+def index():
+    return "QBWC SOAP App is running."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
